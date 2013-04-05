0f1be3f2c1fbed237a69d166a57af6f908b49060
Pietro Di Bello <pietro.dibello@xpeppers.com>
2013-04-06 00:18:28 +0200

    4th solution: adding the discount method on the ghost (singleton) class of the instance object

--- memoize.rb
+++ memoize.rb
@@ -11,21 +11,18 @@ class Discounter
   end
 end
 
-def memoize(cls, method)
-  Class.new(cls) do
-    memory = {}
+d = Discounter.new
 
-    define_method(method) do |*args|
-      if memory.has_key?(args)
-        memory[args]
-      else
-        memory[args] = super(*args)
-      end
-    end
+def d.discount(*skus)
+  @memory ||= {}
+
+  if @memory.has_key?(skus)
+    @memory[skus]
+  else
+    @memory[skus] = super
   end
 end
 
-d = memoize(Discounter, :discount).new
 puts d.discount(1,2,3)
 puts d.discount(1,2,3)
 puts d.discount(1,2,3)
